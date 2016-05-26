d7ab991456f2803e76c792522f56888a2a350d88
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-26 13:33:14 -0700

    Snippet.cpp now re-encodes the video as well.

--- tools/Snippet.cpp
+++ tools/Snippet.cpp
@@ -22,10 +22,18 @@ int main( int argc, char **argv )
 	float fps = capture.get( CV_CAP_PROP_FPS );
 	if( fps > 0 ) {
 		cout << "Video at " << fps << " fps" << endl;
-} else {
-		cout << "Can't get fps for video, using 29.97" << endl;
-		fps = 29.97;
-}
+	} else {
+			cout << "Can't get fps for video, using 29.97" << endl;
+			fps = 29.97;
+	}
+
+	VideoWriter writer( "snippet.mkv", CV_FOURCC('A','V','C','1'), fps,
+										Size(capture.get(CV_CAP_PROP_FRAME_WIDTH),capture.get(CV_CAP_PROP_FRAME_HEIGHT))  );
+
+	if( !writer.isOpened() ) {
+		cerr << "Couldn't create writer" << endl;
+		exit(-1);
+	}
 
 	const int numFrames = int(29.97 * 30);
 	const int seekTo = int(fps * (5*60 + 30));
@@ -33,12 +41,14 @@ int main( int argc, char **argv )
 	int frames = 0;
 	Mat img;
 
-	// seek first
+	// Seek first
 	cout << "Seeking to frame " << seekTo << endl;
 	capture.set( CV_CAP_PROP_POS_FRAMES, seekTo );
 
 	while( capture.read( img ) && frames < numFrames ) {
 		++frames;
+
+			writer << img;
 
 		if( frames % 100 == 0 )
 			cout << "Frame " << frames << endl;
